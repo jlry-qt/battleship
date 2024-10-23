09091cd1868a3ad7185e4fc085531e215176343b
jlry <personal.jlry@gmail.com>
2024-10-23 11:27:30 +0800

    ADDED basic UI with board

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 import './assets/styles/style.css'
+
+const playerBoard = document.querySelector('#pla-board')
+const computerBoard = document.querySelector('#comp-board')
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Generate a 10x10 grid
+    for (let i = 1; i <= 10; i++) {
+        for (let j = 1; j <= 10; j++) {
+            /* eslint-disable */
+            let playerTile = document.createElement('div')
+
+            playerTile.classList.add('tile')
+            playerTile.dataset.x = i
+            playerTile.dataset.y = j
+
+            let computerTile = document.createElement('div')
+
+            computerTile.classList.add('tile')
+            computerTile.dataset.x = i
+            computerTile.dataset.y = j
+
+            playerBoard.appendChild(playerTile)
+            computerBoard.appendChild(computerTile)
+        }
+    }
+})
