6f8635ffbd3f7fe121c50f6f10a9af1358cbb554
jlry <personal.jlry@gmail.com>
2024-10-23 21:58:02 +0800

    WIP

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const computerBoard = document.querySelector('#comp-board')
 
 document.addEventListener('DOMContentLoaded', () => {
     // Generate a 10x10 grid
-    for (let i = 1; i <= 10; i++) {
+    for (let i = 0; i <= 9; i++) {
         for (let j = 1; j <= 10; j++) {
             /* eslint-disable */
             let playerTile = document.createElement('div')
