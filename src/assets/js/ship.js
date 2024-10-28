810158b493760aeda68eac1255a16257e995e2b7
jlry-qt <jlry.personal@gmail.com>
2024-10-28 20:11:09 +0800

    Separated ship config as JSON

--- src/assets/js/ship.js
+++ src/assets/js/ship.js
@@ -1,46 +1,10 @@
-const shipConfig = (() => {
-    const shipType = new Map()
-    shipType.set(1, {
-        len: 5,
-        class: 'Carrier',
-    })
-
-    shipType.set(2, {
-        len: 4,
-        class: 'Battleship',
-    })
-
-    shipType.set(3, {
-        len: 3,
-        class: 'Destroyer',
-    })
-
-    shipType.set(4, {
-        len: 3,
-        class: 'Submarine',
-    })
-
-    shipType.set(5, {
-        len: 2,
-        class: 'Patrol Boat',
-    })
-
-    function getLength(id) {
-        return shipType.get(id).len
-    }
-
-    function getClass(id) {
-        return shipType.get(id).class
-    }
-
-    return { getLength, getClass }
-})()
+import shipConfig from '../static/ship-config.json'
 
 export class Ship {
     constructor(id) {
         this.id = id
-        this.shipLength = shipConfig.getLength(id)
-        this.class = shipConfig.getClass(id)
+        this.shipLength = shipConfig[id].len
+        this.class = shipConfig[id].class
         this.sunk = false
         this.hits = 0
     }
